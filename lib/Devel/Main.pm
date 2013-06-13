64a395cad11193333b712173e63c2b37fd1cb760
Stephen Nelson <stephenenelson@mac.com>
2013-06-13 01:00:26 -0700

    Change to less funky package syntax so that PAUSE indexer can (one hopes) read it

--- lib/Devel/Main.pm
+++ lib/Devel/Main.pm
@@ -2,50 +2,48 @@
 use strict;
 use warnings;
 
-package Devel::Main {
+package Devel::Main;
 
-    # We use Sub::Exporter so you can import main with different names
-    # with 'use Devel::Main 'main' => { -as => 'other' }
-    use Sub::Exporter 0.985;
-    Sub::Exporter::setup_exporter(
-        {
-            exports => {
-                'main' => \&main_generator
-            }
+# We use Sub::Exporter so you can import main with different names
+# with 'use Devel::Main 'main' => { -as => 'other' }
+use Sub::Exporter 0.985;
+Sub::Exporter::setup_exporter(
+    {
+        exports => {
+            'main' => \&main_generator
         }
-    );
-
-    sub main_generator {
-        my ( $class, $name, $args ) = @_;
-
-        my $run_sub_name = $args->{'run_sub_name'} // "run_$name";
-        my $exit         = $args->{'exit'}         // 1;
-
-        return sub (&) {
-            my ($main_sub) = @_;
-
-            # If we're called from a script, run main and exit
-            if ( !defined caller(1) ) {
-                $main_sub->();
-                exit(0) if $exit;
-            }
-
-            # Otherwise, create a sub that turns its arguments into @ARGV
-            else {
-                no strict 'refs';
-                my $package = caller;
-                *{"${package}::$run_sub_name"} = sub {
-                    local @ARGV = @_;
-                    return $main_sub->();
-                };
-
-                # Return 1 to make the script pass 'require'
-                return 1;
-            }
-        };
     }
+);
 
-};
+sub main_generator {
+    my ( $class, $name, $args ) = @_;
+
+    my $run_sub_name = $args->{'run_sub_name'} // "run_$name";
+    my $exit         = $args->{'exit'}         // 1;
+
+    return sub (&) {
+        my ($main_sub) = @_;
+
+        # If we're called from a script, run main and exit
+        if ( !defined caller(1) ) {
+            $main_sub->();
+            exit(0) if $exit;
+        }
+
+        # Otherwise, create a sub that turns its arguments into @ARGV
+        else {
+            no strict 'refs';
+            my $package = caller;
+            *{"${package}::$run_sub_name"} = sub {
+                local @ARGV = @_;
+                return $main_sub->();
+            };
+
+            # Return 1 to make the script pass 'require'
+            return 1;
+        }
+    };
+}
 
 1;
 
